b5074596122e8efefcabc9336c5700a70093fc81
github username <github email address>
2019-07-09 18:46:57 -0400

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
 class Users extends Component {
-
+  //BUGS: use console.log(name of key, this.props.<path to key>)
+  //BUGS: surround console.log with console.group (google or see syntax below)
+  //BUGS: debugger
   render() {
-    // debugger
+    console.group(`Inside the user component`)
+      console.log('username', this.props.user.username);
+      console.log('hometown', this.props.user.hometown);
+    console.groupEnd()
+
+    let users = this.props.users.map((user, index) => <li key={index}>{user.username}</li>)
+    debugger
     return (
       <div>
         <ul>
-          {this.props.users.map((user, index) => (<li key={index}>{user}</li>}))
+          {users}
         </ul>
       </div>
     )
