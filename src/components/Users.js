ae835c1be6130908e7c217c78bebb6c0dc287562
github username <github email address>
2019-07-09 19:11:22 -0400

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -5,17 +5,17 @@ class Users extends Component {
   //BUGS: use console.log(name of key, this.props.<path to key>)
   //BUGS: surround console.log with console.group (google or see syntax below)
   //BUGS: debugger
+  // console.group(`Inside the user component`)
+  //   console.log('username', this.props.user.username);
+  //   console.log('hometown', this.props.user.hometown);
+  // console.groupEnd()
   render() {
-    // console.group(`Inside the user component`)
-    //   console.log('username', this.props.user.username);
-    //   console.log('hometown', this.props.user.hometown);
-    // console.groupEnd()
 
     let users = this.props.users.map((user, index) => <li key={index}>{user.username}</li>)
     debugger
     return (
       <div>
-        {this.state.users.length}
+        {this.props.users.length}
         <ul>
           {users}
         </ul>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import manageUsers from './reducers/manageUsers'
 import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 
-let store = createStore(manageUsers)
+let store = createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
   <Provider store={store}>
