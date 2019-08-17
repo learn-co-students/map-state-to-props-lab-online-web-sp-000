cfd60431d9f922b02de44c93103ea979f6a7cf41
Mccaryr <robert.mccary@yahoo.com>
2019-08-17 13:55:31 -0700

    Test issues, unable to resolve

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import manageUsers from './reducers/manageUsers'
+
+const store = creaeteStore(manageUsers)
 
 
 // add imports and code
@@ -10,7 +13,9 @@ import manageUsers from './reducers/manageUsers'
 
 ReactDOM.render(
   // add imports and code
+  <Provider store={store}>
   <App />
+  </Provider>
   // add imports and code
   ,
   document.getElementById('root')
