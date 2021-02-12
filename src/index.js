3d94f54f1df5f12218b81fa5ce25f986a0de774c
Sergio Marelli <sergio.mrll@gmail.com>
2021-02-12 14:59:35 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import manageUsers from './reducers/manageUsers'
 
 
 // add imports and code
 
+let storello = createStore(manageUsers )
+
 
 ReactDOM.render(
   // add imports and code
+  <Provider store={storello}>
   <App />
+  </Provider>
   // add imports and code
   ,
   document.getElementById('root')
