287e987e5a177dbfb4befe723ce1b9007deb33e3
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-26 16:35:37 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App'
 
 import manageUsers from './reducers/manageUsers'
@@ -7,10 +9,14 @@ import manageUsers from './reducers/manageUsers'
 
 // add imports and code
 
+let store = createStore(manageUsers)
+
 
 ReactDOM.render(
   // add imports and code
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
   // add imports and code
   ,
   document.getElementById('root')
